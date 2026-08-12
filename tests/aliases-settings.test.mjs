e5e073e4f21f7160ea78de2216c242cc20453ac7
Eli Sterling <eli@eliware.org>
2026-08-12 04:00:49 +0000

    Make permissions test cross platform

--- tests/aliases-settings.test.mjs
+++ tests/aliases-settings.test.mjs
@@ -8,7 +8,7 @@ test('aliases and settings persist with private files', () => {
   const home = fs.mkdtempSync(path.join(os.tmpdir(), 'cf-config-'));
   writeAliases({ zones: 'zone list' }, home); writeSettings({ pager: 'less' }, home);
   expect(readAliases(home)).toEqual({ zones: 'zone list' }); expect(readSettings(home)).toEqual({ pager: 'less' });
-  expect((fs.statSync(`${home}/.config/cf/aliases.json`).mode & 0o777).toString(8)).toBe('600');
+  if (process.platform !== 'win32') expect((fs.statSync(`${home}/.config/cf/aliases.json`).mode & 0o777).toString(8)).toBe('600');
   expect(readAliases(home, {})).toEqual({}); expect(readSettings(home, {})).toEqual({});
 });
 
